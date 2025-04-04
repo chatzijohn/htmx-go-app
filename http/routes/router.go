631b74eaa48133aa20b7c288f6015cb505f0a82c
pchatzigiannis <pchatzigiannis@technor.gr>
2025-04-04 17:01:44 +0300

    feat: refactored server logic

--- http/routes/router.go
+++ http/routes/router.go
@@ -11,7 +11,7 @@ import (
 type HttpRouter struct {
 	Cancel  context.CancelFunc
 	Context context.Context
-	Config  *config.ServerConfig
+	Config  *config.AppConfig
 }
 
 func NewRouter() *HttpRouter {
@@ -23,8 +23,8 @@ func NewRouter() *HttpRouter {
 	mux.HandleFunc("GET /", handlers.CountryList)
 	mux.HandleFunc("GET /country/{name}", handlers.CountryDetail)
 
-	log.Printf("Server listening on :%s", cfg.PORT)
-	log.Fatal(http.ListenAndServe(":"+cfg.PORT, mux))
+	log.Printf("Server listening on :%s", cfg.SERVER.PORT)
+	log.Fatal(http.ListenAndServe(":"+cfg.SERVER.PORT, mux))
 
 	return &HttpRouter{}
 }
