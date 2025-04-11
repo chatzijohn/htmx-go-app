fc0eac1321f80cbd2b2c3a2e4976916be12a682c
Panagiotis Chatzigiannis <pchatzijohn@gmail.com>
2025-04-12 01:55:55 +0300

    feat: added static file serving

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,6 +10,11 @@ import (
 func New(serverConfig *config.ServerConfig, services *service.ServiceContainer) http.Handler {
 	mux := http.NewServeMux()
 
+	// Serve static files (CSS, JS, etc.)
+	staticFs := http.FileServer(http.Dir("./web/static"))
+	mux.Handle("/web/static/", http.StripPrefix("/web/static/", staticFs))
+
+	// Your other route(s)
 	countryRoutes := routes.NewCountryRoutes(services.CountryService)
 	mux.Handle("/countries/", http.StripPrefix("/countries", countryRoutes.Register()))
 
--- web/components/countries_templ.go
+++ web/components/countries_templ.go
@@ -31,7 +31,7 @@ func Countries(countries []*models.Country) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"p-6 max-w-4xl mx-auto bg-red-100\"><h1 class=\"text-2xl font-bold mb-4\">Countries</h1><div class=\"grid gap-4 sm:grid-cols-2 md:grid-cols-3\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"p-6 max-w-4xl mx-auto\"><h1 class=\"text-2xl font-bold mb-4\">Countries</h1><div class=\"grid gap-4 sm:grid-cols-2 md:grid-cols-3\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
